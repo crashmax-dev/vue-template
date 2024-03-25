44ef5565994deb2583498c0d953ccb7768d0829c
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-03-25 22:14:57 +0800

    chore: notice publish to npm

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,9 +1,23 @@
+/**
+ * To publish the package to npm
+ * you will need to specify exports in package.json
+ *
+ * "exports": {
+ *   ".": {
+ *     "types": "./dist/index.d.ts",
+ *     "import": "./dist/utils.js",
+ *     "require": "./dist/utils.umd.cjs"
+ *   }
+ * }
+ */
+
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [dts()],
   build: {
+    target: 'esnext',
     sourcemap: true,
     minify: false,
     emptyOutDir: false,
