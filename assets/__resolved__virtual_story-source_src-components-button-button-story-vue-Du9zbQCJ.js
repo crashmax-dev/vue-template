f23b242204cf317db57c15a14ddb0da5754e79b9
crashmax-dev <crashmax.dev@gmail.com>
2025-01-01 18:51:44 +0000

    Deploying to gh-pages from @ crashmax-dev/vue-template@2e2fcf221f2906787cba8b75d5ce3866b5403648 🚀

---
+++ assets/__resolved__virtual_story-source_src-components-button-button-story-vue-Du9zbQCJ.js
@@ -0,0 +1,24 @@
+const __resolved__virtual_storySource_srcComponentsButtonButtonStoryVue = `<template>
+  <Story title="Button" auto-props-disabled>
+    <VButton variant="primary">
+      Primary
+    </VButton>
+    <VButton variant="secondary">
+      Secondary
+    </VButton>
+    <VButton variant="danger">
+      Danger
+    </VButton>
+    <VButton variant="secondary" disabled>
+      Disabled
+    </VButton>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import VButton from './button.vue'
+<\/script>
+`;
+export {
+  __resolved__virtual_storySource_srcComponentsButtonButtonStoryVue as default
+};
--- assets/index-DKwD6Df4.js
+++
@@ -1,1 +0,0 @@
-import{M,u as a}from"./index-C7AkseiM.js";import"./index-BZ0IjMk6.js";import"./iframe-DJtebQgp.js";import"../sb-preview/runtime.js";import"./index-ZlVIvrTI.js";import"./index-D-8MO0q_.js";import"./index-DrFu-skq.js";export{M as MDXProvider,a as useMDXComponents};
