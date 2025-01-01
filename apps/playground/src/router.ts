3966f4ea11a2c85012372e4e79712caf950e8a37
Vitalij Ryndin <crashmax.dev@gmail.com>
2025-01-02 03:09:01 +0800

    fix: router

--- apps/playground/src/router.ts
+++ apps/playground/src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 export const RoutePath = {
   Home: '/',
@@ -6,7 +6,7 @@ export const RoutePath = {
 } as const
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: RoutePath.Home,
