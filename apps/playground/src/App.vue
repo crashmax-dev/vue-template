ed7968b601a843a317179d9622dc7b6ed1d5340a
Vitalij Ryndin <crashmax.dev@gmail.com>
2025-01-02 02:18:15 +0800

    feat: container component

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
 import { VThemeProvider } from '@vue-workspace/ui'
-import { RouterView } from 'vue-router'
+import Container from './components/container.vue'
 import Header from './components/header.vue'
 </script>
 
 <template>
   <VThemeProvider initial-theme="dark">
     <Header />
-    <div class="container">
-      <RouterView />
-    </div>
+    <Container />
   </VThemeProvider>
 </template>
-
-<style scoped>
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 1rem;
-}
-</style>
---
+++ apps/playground/src/components/container.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+}
+</style>
