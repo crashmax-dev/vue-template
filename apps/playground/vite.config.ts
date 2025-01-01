506b0b018f24857885e029ba3cf13899db4718b8
Vitalij Ryndin <crashmax.dev@gmail.com>
2025-01-02 04:46:03 +0800

    feat: importmap for keep file hashes

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,9 +1,13 @@
 import { alias, css, plugins } from '@vue-workspace/vite-config'
 import { defineConfig } from 'vite'
+import { staticFilehashPlugin } from 'vite-plugin-static-filehash'
 
 export default defineConfig({
   base: './',
-  plugins,
+  plugins: [
+    ...plugins,
+    staticFilehashPlugin(),
+  ],
   css,
   resolve: {
     alias: alias(import.meta.url),
