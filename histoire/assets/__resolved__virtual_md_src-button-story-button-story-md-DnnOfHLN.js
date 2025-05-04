1a5553254927764c6fb0a0c9205e64c91afad8fa
crashmax-dev <crashmax.dev@gmail.com>
2025-05-04 07:24:50 +0000

    Deploying to gh-pages from @ crashmax-dev/vue-template@c88b0095ef5335f16ccc1c3c34048f276abaf5ca 🚀

---
+++ histoire/assets/__resolved__virtual_md_src-button-story-button-story-md-DnnOfHLN.js
@@ -0,0 +1,17 @@
+const html = `<h3 id="usage" tabindex="-1">Usage <a class="header-anchor" href="#usage" aria-hidden="true">#</a></h3>
+<pre><code class="language-vue"><div class="htw-relative htw-not-prose __histoire-code"><div class="htw-absolute htw-top-0 htw-right-0 htw-text-xs htw-text-white/40">vue</div><pre class="shiki github-dark" style="background-color:#24292e;color:#e1e4e8" tabindex="0"><code><span class="line"><span style="color:#E1E4E8">&#x3C;</span><span style="color:#85E89D">template</span><span style="color:#E1E4E8">></span></span>
+<span class="line"><span style="color:#E1E4E8">  &#x3C;</span><span style="color:#85E89D">Button</span><span style="color:#E1E4E8">>Button&#x3C;/</span><span style="color:#85E89D">Button</span><span style="color:#E1E4E8">></span></span>
+<span class="line"><span style="color:#E1E4E8">&#x3C;/</span><span style="color:#85E89D">template</span><span style="color:#E1E4E8">></span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8">&#x3C;</span><span style="color:#85E89D">script</span><span style="color:#B392F0"> setup</span><span style="color:#B392F0"> lang</span><span style="color:#E1E4E8">=</span><span style="color:#9ECBFF">"ts"</span><span style="color:#E1E4E8">></span></span>
+<span class="line"><span style="color:#F97583">import</span><span style="color:#E1E4E8"> { Button } </span><span style="color:#F97583">from</span><span style="color:#9ECBFF"> '@vue-workspace/ui'</span></span>
+<span class="line"><span style="color:#E1E4E8">&#x3C;/</span><span style="color:#85E89D">script</span><span style="color:#E1E4E8">></span></span>
+<span class="line"></span></code></pre></div></code></pre>
+`;
+const frontmatter = {};
+const relativePath = "src/button/story/button.story.md";
+export {
+  frontmatter,
+  html,
+  relativePath
+};
