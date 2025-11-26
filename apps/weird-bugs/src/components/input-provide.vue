ca351a583585e0ab9d5a4627dedfacf79496cd3b
Vitalij Ryndin <crashmax.dev@gmail.com>
2025-11-26 18:42:43 +0300

    feat: weird bugs

---
+++ apps/weird-bugs/src/components/input-provide.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    v-model="value"
+    placeholder="Type text..."
+    @update:model-value="provide(prevValue, value)"
+  >
+</template>
+
+<script setup lang="ts" generic="T extends unknown">
+type ProvideFn<T> = (prevValue: T, value: T) => void
+
+defineProps<{
+  provide?: ProvideFn<T>
+}>()
+
+const value = defineModel<T>({ required: true })
+const prevValue = usePrevious(value)
+</script>
