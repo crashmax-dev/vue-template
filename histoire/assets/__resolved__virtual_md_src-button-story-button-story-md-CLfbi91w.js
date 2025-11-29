8c332c5c4cffa21b7601c0e5681c5ef733843902
crashmax-dev <crashmax.dev@gmail.com>
2025-11-29 23:49:28 +0000

    Deploying to gh-pages from @ crashmax-dev/vue-template@97c424590ad6a9532e7d173c088e41338a6aed2d 🚀

---
+++ histoire/assets/__resolved__virtual_md_src-button-story-button-story-md-CLfbi91w.js
@@ -0,0 +1,8 @@
+const html = '<h3 id="usage" tabindex="-1">Usage <a class="header-anchor" href="#usage" aria-hidden="true">#</a></h3>\n<pre><code class="language-vue"><div class="htw-relative htw-not-prose __histoire-code"><div class="htw-absolute htw-top-0 htw-right-0 htw-text-xs htw-text-white/40">vue</div><pre class="shiki github-dark" style="background-color:#24292e;color:#e1e4e8" tabindex="0"><code><span class="line"><span style="color:#E1E4E8">&#x3C;</span><span style="color:#85E89D">template</span><span style="color:#E1E4E8">></span></span>\n<span class="line"><span style="color:#E1E4E8">  &#x3C;</span><span style="color:#85E89D">button</span><span style="color:#E1E4E8">>Button&#x3C;/</span><span style="color:#85E89D">button</span><span style="color:#E1E4E8">></span></span>\n<span class="line"><span style="color:#E1E4E8">&#x3C;/</span><span style="color:#85E89D">template</span><span style="color:#E1E4E8">></span></span>\n<span class="line"></span>\n<span class="line"><span style="color:#E1E4E8">&#x3C;</span><span style="color:#85E89D">script</span><span style="color:#B392F0"> setup</span><span style="color:#B392F0"> lang</span><span style="color:#E1E4E8">=</span><span style="color:#9ECBFF">"ts"</span><span style="color:#E1E4E8">></span></span>\n<span class="line"><span style="color:#E1E4E8">&#x3C;/</span><span style="color:#85E89D">script</span><span style="color:#E1E4E8">></span></span>\n<span class="line"></span></code></pre></div></code></pre>\n';
+const frontmatter = {};
+const relativePath = "src/button/story/button.story.md";
+export {
+  frontmatter,
+  html,
+  relativePath
+};
---
+++ histoire/assets/__resolved__virtual_story-source_src-button-story-button-story-vue-C_xq5dZh.js
@@ -0,0 +1,41 @@
+const __resolved__virtual_storySource_srcButtonStoryButtonStoryVue = `<template>
+  <Story title="Button">
+    <Variant title="Variants">
+      <v-button variant="primary">
+        Primary
+      </v-button>
+      <v-button variant="secondary">
+        Secondary
+      </v-button>
+      <v-button variant="danger">
+        Danger
+      </v-button>
+      <v-button
+        variant="secondary"
+        disabled
+      >
+        Disabled
+      </v-button>
+    </Variant>
+
+    <Variant title="Sizes">
+      <v-button
+        v-for="size of sizes"
+        :key="size"
+        :size="size"
+      >
+        {{ size }}
+      </v-button>
+    </Variant>
+  </Story>
+</template>
+
+<script setup lang="ts">
+import VButton from '../button.vue'
+
+const sizes = ['medium', 'small'] as const
+<\/script>
+`;
+export {
+  __resolved__virtual_storySource_srcButtonStoryButtonStoryVue as default
+};
