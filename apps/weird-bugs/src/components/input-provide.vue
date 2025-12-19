7c38ac6314c2e548260aa804b54861057ebec20b
Vitalij Ryndin <crashmax.dev@gmail.com>
2025-12-19 20:07:14 +0300

    chore: component auto import fixed

--- apps/weird-bugs/src/components/input-provide.vue
+++ apps/weird-bugs/src/components/input-provide.vue
@@ -2,14 +2,18 @@
   <input
     v-model="value"
     placeholder="Type text..."
-    @update:model-value="provide(prevValue, value)"
+    :readonly="readonly"
+    @update:model-value="provide?.(value, prevValue)"
   >
 </template>
 
-<script setup lang="ts" generic="T extends unknown">
-type ProvideFn<T> = (prevValue: T, value: T) => void
+<script setup lang="ts" generic="T extends any">
+// Fixed https://github.com/vuejs/language-tools/pull/5790
+
+type ProvideFn<T> = (value: T, prevValue?: T) => void
 
 defineProps<{
+  readonly?: boolean
   provide?: ProvideFn<T>
 }>()
 
