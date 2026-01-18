1346d1afeca6510722baf94ee97ef83cb988c163
crashmax-dev <crashmax.dev@gmail.com>
2026-01-18 13:27:44 +0000

    Deploying to gh-pages from @ crashmax-dev/vue-template@4ab657eb55ef6adfdea8b994dd26de3eb9cd584b 🚀

---
+++ histoire/assets/search-docs-data-Bj0nAexb.js
@@ -0,0 +1,9 @@
+import { W as markRaw } from "./vendor-BUrfcTMX.js";
+let searchData$1 = { "index": [{ "id": 0, "text": 'Usage #\nvue<template>\n  <button>Button</button>\n</template>\n\n<script setup lang="ts">\n<\/script>\n\n' }], "idMap": { "0": { "id": "src-button-story-button-story-vue", "kind": "story" } } };
+const searchData = markRaw(searchData$1);
+function onUpdate(cb) {
+}
+export {
+  onUpdate,
+  searchData
+};
